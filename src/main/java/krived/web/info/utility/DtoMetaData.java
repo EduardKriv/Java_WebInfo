0e694366a69ccf1b1ca7749d1f83bf2b580e1266
Capers Nathalie <cnathali@in-d4.nsk.21-school.ru>
2023-08-27 20:39:04 +0700

    some

--- src/main/java/krived/web/info/utility/DtoMetaData.java
+++ src/main/java/krived/web/info/utility/DtoMetaData.java
@@ -2,12 +2,13 @@ package krived.web.info.utility;
 
 import com.opencsv.bean.CsvBindByName;
 import krived.web.info.model.dto.CsvBean;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class DtoMetaData {
-    public static <T extends CsvBean> List<String> getColumnNames(Class<T> clazz) {
+    public static <T extends CsvBean> List<String> getColumnNames(@NotNull Class<T> clazz) {
         return Arrays.stream(clazz.getDeclaredFields())
                 .map(field -> field.getDeclaredAnnotation(CsvBindByName.class))
                 .map(CsvBindByName::column)
